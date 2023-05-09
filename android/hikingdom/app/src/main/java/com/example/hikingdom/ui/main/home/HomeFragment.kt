31454cc95ddfe95ad6ce87737357afd4326c783f
kjyk0835 <kjyk0835@naver.com>
2023-05-09 18:06:50 +0900

    AND:fix: add webview settings: MIXED_CONTENT_ALWAYS_ALLOW

--- android/hikingdom/app/src/main/java/com/example/hikingdom/ui/main/home/HomeFragment.kt
+++ android/hikingdom/app/src/main/java/com/example/hikingdom/ui/main/home/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.example.hikingdom.ui.main.home
 
+import android.os.Build
+import android.webkit.WebSettings
 import android.webkit.WebViewClient
 import androidx.activity.OnBackPressedCallback
 import com.example.hikingdom.ApplicationClass.Companion.HOME_WEB_URL
 import com.example.hikingdom.databinding.FragmentHomeBinding
 import com.example.hikingdom.ui.BaseFragment
+
 
 class HomeFragment(): BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
 
@@ -22,6 +25,11 @@ class HomeFragment(): BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inf
         webView.loadUrl(HOME_WEB_URL)
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
+
+        val wsetting: WebSettings = webView.settings
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) { // https 이미지.
+            wsetting.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        }
 
         activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
