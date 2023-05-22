a955b485b7a82f4336f4c53bf1506cfdf47115aa
teamzamong <teamzamong@gmail.com>
2023-05-22 18:03:05 +0900

    BE:refactor: add JavaDoc for MemberManagementController

--- backend/service/src/main/java/org/lightnsalt/hikingdom/service/member/controller/MemberManagementController.java
+++ backend/service/src/main/java/org/lightnsalt/hikingdom/service/member/controller/MemberManagementController.java
@@ -35,7 +35,7 @@ import org.springframework.web.multipart.MultipartFile;
 import lombok.RequiredArgsConstructor;
 
 /**
- * 회원 관리와 관련된 API
+ * 사용자 관리와 관련된 API
  * JWT 토큰 인증 필요
  */
 @RestController
@@ -44,12 +44,24 @@ import lombok.RequiredArgsConstructor;
 public class MemberManagementController {
 	private final MemberManagementService memberManagementService;
 
+	/**
+	 * 로그인한 사용자의 상세 정보 조회 요청을 처리한다.
+	 *
+	 * @param authentication 현재 인증된 사용자의 인증 객체
+	 * @return 로그인한 사용자의 상세 정보를 포함한 응답
+	 */
 	@GetMapping
 	public ResponseEntity<CustomResponseBody> memberInfoDetail(Authentication authentication) {
 		MemberDetailRes memberDetailRes = memberManagementService.findMemberInfo(authentication.getName());
 		return new ResponseEntity<>(BaseResponseBody.of("회원 정보 조회에 성공했습니다", memberDetailRes), HttpStatus.OK);
 	}
 
+	/**
+	 * 사용자 탈퇴 요청을 처리한다.
+	 *
+	 * @param authentication 현재 인증된 사용자의 인증 객체
+	 * @return 사용자 탈퇴 처리 상태에 대한 응답
+	 */
 	@DeleteMapping("/withdraw")
 	public ResponseEntity<CustomResponseBody> memberRemove(Authentication authentication) {
 		memberManagementService.removeMember(authentication.getName());
@@ -57,6 +69,13 @@ public class MemberManagementController {
 		return new ResponseEntity<>(BaseResponseBody.of("회원 탈퇴에 성공했습니다"), HttpStatus.OK);
 	}
 
+	/**
+	 * 로그아웃 요청을 처리한다.
+	 *
+	 * @param bearerToken 사용자 인증을 위한 Bearer token
+	 * @param memberLogoutReq 로그아웃 요청 객체
+	 * @return 로그아웃 처리 상태에 대한 응답
+	 */
 	@PostMapping("/logout")
 	public ResponseEntity<CustomResponseBody> logout(@RequestHeader("Authorization") String bearerToken,
 		@RequestBody(required = false) MemberLogoutReq memberLogoutReq) {
@@ -65,6 +84,14 @@ public class MemberManagementController {
 		return new ResponseEntity<>(BaseResponseBody.of("로그아웃에 성공했습니다"), HttpStatus.OK);
 	}
 
+	/**
+	 * 비밀번호 변경 요청을 처리한다.
+	 *
+	 * @param authentication 현재 인증된 사용자의 인증 객체
+	 * @param memberChangePasswordReq 비밀번호 변경 요청 객체
+	 * @param bindingResult 비밀번호 변경 요청 객체에 대한 유효성 검사 결과
+	 * @return 비밀번호 변경 요청 처리 상태에 대한 응답
+	 */
 	@PutMapping("/password-change")
 	public ResponseEntity<CustomResponseBody> passwordChange(Authentication authentication,
 		@RequestBody @Valid MemberChangePasswordReq memberChangePasswordReq, BindingResult bindingResult) {
@@ -79,10 +106,17 @@ public class MemberManagementController {
 		return new ResponseEntity<>(BaseResponseBody.of("비밀번호 변경에 성공했습니다"), HttpStatus.OK);
 	}
 
+	/**
+	 * 닉네임 변경 요청을 처리한다.
+	 *
+	 * @param authentication 현재 인증된 사용자의 인증 객체
+	 * @param memberNicknameReq 닉네임 변경 요청 객체
+	 * @param bindingResult 닉네임 변경 요청 객체에 대한 유효성 검사 결과
+	 * @return 닉네임 변경 요청 처리 상태에 대한 응답
+	 */
 	@PutMapping("/nickname-change")
 	public ResponseEntity<CustomResponseBody> nicknameChange(Authentication authentication,
-		@RequestBody @Valid MemberNicknameReq memberNicknameReq,
-		BindingResult bindingResult) {
+		@RequestBody @Valid MemberNicknameReq memberNicknameReq, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			return new ResponseEntity<>(ErrorResponseBody.of(ErrorCode.INVALID_INPUT_VALUE,
 				bindingResult.getAllErrors().get(0).getDefaultMessage()),
@@ -94,6 +128,13 @@ public class MemberManagementController {
 		return new ResponseEntity<>(BaseResponseBody.of("닉네임 변경에 성공했습니다"), HttpStatus.OK);
 	}
 
+	/**
+	 * 프로필 이미지 변경 요청을 처리한다.
+	 *
+	 * @param authentication 현재 인증된 사용자의 인증 객체
+	 * @param image 변경할 프로필 이미지
+	 * @return 변경된 프로필 이미지의 URL을 포함한 응답
+	 */
 	@PutMapping("/profile-image-change")
 	public ResponseEntity<CustomResponseBody> profileImageChange(Authentication authentication,
 		@RequestBody MultipartFile image) {
@@ -102,6 +143,14 @@ public class MemberManagementController {
 		return new ResponseEntity<>(BaseResponseBody.of("프로필 사진 변경에 성공했습니다", profileUrl), HttpStatus.OK);
 	}
 
+	/**
+	 * 사용자 프로필 상세 조회를 처리한다.
+	 *
+	 * @param authentication 현재 인증된 사용자의 인증 객체
+	 * @param nickname 조회할 사용자의 닉네임
+	 * @param pageable 페이지네이션 정보를 포함하는 Pageable 객체 (필수 X)
+	 * @return 사용자 프로필 정보를 포함한 응답
+	 */
 	@GetMapping("{nickname}")
 	public ResponseEntity<CustomResponseBody> profileDetail(Authentication authentication,
 		@PathVariable String nickname, @PageableDefault(value = 3) Pageable pageable) {
@@ -109,6 +158,12 @@ public class MemberManagementController {
 		return new ResponseEntity<>(BaseResponseBody.of("회원 프로필 조회에 성공했습니다", result), HttpStatus.OK);
 	}
 
+	/**
+	 * 현재 사용자가 가입 대기 중인 소모임 목록을 조회한다.
+	 *
+	 * @param authentication 현재 인증된 사용자의 인증 객체
+	 * @return 가입 대기 중인 소모임 목록을 포함한 응답
+	 */
 	@GetMapping("/clubs/my-requests")
 	public ResponseEntity<CustomResponseBody> requestClubList(Authentication authentication) {
 		List<MemberRequestClubRes> result = memberManagementService.findRequestClub(authentication.getName());
--- backend/service/src/main/java/org/lightnsalt/hikingdom/service/member/dto/request/MemberLogoutReq.java
+++ backend/service/src/main/java/org/lightnsalt/hikingdom/service/member/dto/request/MemberLogoutReq.java
@@ -2,7 +2,14 @@ package org.lightnsalt.hikingdom.service.member.dto.request;
 
 import lombok.Data;
 
+/**
+ * 사용자 로그아웃 요청 객체
+ */
 @Data
 public class MemberLogoutReq {
+
+	/**
+	 * 사용자 기기의 Firebase Cloud Messaging(FCM) 토큰
+	 */
 	private String fcmToken;
 }
